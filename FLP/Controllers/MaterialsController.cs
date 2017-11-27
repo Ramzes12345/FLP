8b93d301c55b983cd9472520604a3b313ec7a4e4
Ramzes12345 <lipinskiy.development@gmail.com>
2017-11-27 16:45:51 +0200

    Initial commit

---
+++ FLP/Controllers/MaterialsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using FLP.Models;
+
+namespace FLP.Controllers
+{
+    public class MaterialsController : Controller
+    {
+
+        public ActionResult WiFiMaterial()
+        {
+            return View();
+        }
+        public ActionResult Uslugi()
+        {
+            return View();
+        }
+        public ActionResult Qestions()
+        {
+            return View();
+        }
+        public ActionResult NetworksMaterial()
+        {
+            return View();
+        }
+        public ActionResult BildingMaterial()
+        {
+            return View();
+        }
+        public ActionResult SignalMaterial()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FLP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FLP.Startup))]
+namespace FLP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
